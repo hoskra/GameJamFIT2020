1dd9f652f100b4d3432b464cae0b6af4b3dc2477
Adam Vesecký <vesecky.adam@gmail.com>
2020-02-15 13:34:34 +0100

    Update gitignore

--- build/game.45c3c8f3.js
+++ build/game.45c3c8f3.js
@@ -43877,7 +43877,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52697" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55561" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
