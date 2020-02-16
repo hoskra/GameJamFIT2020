9c853cc93c713f4b827c82603e3caa3f9fc41027
Adam Vesecký <vesecky.adam@gmail.com>
2020-02-16 10:50:24 +0100

    Fix bugs

--- src/animators/move-anim.ts
+++ src/animators/move-anim.ts
@@ -62,12 +62,23 @@ export class MoveAnim extends BaseAnimator {
 
       this.object.pixiObj.position.set(this.object.pixiObj.position.x + diffX * this.speed * delta * 0.01, this.object.pixiObj.position.y + diffY * this.speed * delta * 0.01);
       
+      if(this.lastDistance == 0) {
+        this.lastDistance = new Vec(this.object.pixiObj.position.x, this.object.pixiObj.position.y).distance(this.targetCell.multiply(BLOCK_SIZE));
+      }
 
       // check alignments
       if(helpers.isAlmostAtCell(new Vec(this.object.pixiObj.position.x, this.object.pixiObj.position.y), this.targetCell)) {
         helpers.alignToCell(this.object, this.targetCell);
         this.isRunning = false;
         this.onComplete();
+      } else {
+        let distance = new Vec(this.object.pixiObj.position.x, this.object.pixiObj.position.y).distance(this.targetCell.multiply(BLOCK_SIZE));
+        if(this.lastDistance < distance) {
+          helpers.alignToCell(this.object, this.targetCell);
+          this.isRunning = false;
+          this.onComplete();
+        }
+        this.lastDistance = distance;
       }
     }
   }
